909488b51b4066356a9858d5483427d84424b553
kbraunwart <119632119+kbraunwart@users.noreply.github.com>
2023-04-28 12:20:22 -0400

    Update art.js
    Add Einstein details to art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Einstein",
+   "filename": "einstein.txt",
+   "credit_text": "jgs",
+   "credit_url": "https://www.asciiart.eu/people/famous/albert-einstein"},
 ];
