e19d48f23b2ef5e88a0f7986515de60afb562ce8
Lswiggard <78099004+Lswiggard@users.noreply.github.com>
2023-04-28 12:07:25 -0400

    Update art.js
    I added catbox to the art folder

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Cat",
+   "filename": "CatBox.txt",
+   "credit_text": "HJS",
+   "credit_url": "https://www.asciiart.eu/animals/cats"},
 ];
